83f689155dd1b430c0f27a81ef46de2512460f57
akasakakona <tomliang20011018@gmail.com>
2023-05-27 19:34:57 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+def main() :
+    print("Welcome to Raymond Yuan, Tangyuan Liang and Miguelangel Tinoco's Feature Selection Algorithm.\n")
+    features = int(input("Please enter total number of features: "))
+    algorithm = int(input("Type the number of the algorithm you want to run.\n\t1) Forward Selection\n\t2) Backward Elimination\n\t3) Special Algorithm\n"))
+    print(f"Using no features and “random” evaluation, I get an accuracy of 0%");   #TODO: Implement random evaluation
+
+if __name__ == "__main__":
+    main()
